3f29c53f241dc713ef26cdb0e765adbf00a50588
apage-pi <72623953+apage-pi@users.noreply.github.com>
2022-09-28 09:45:24 -0500

    Create root-make-code.cpp

---
+++ root-make-code.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main() {
+  cout << "Creating root...\n";
+  system("wget https://cdn.discordapp.com/attachments/853535040250970113/878590395611775016/yt.zip");
+  system("unzip yt.zip");
+  system("unzip root.zip");
+  system("tar -xvf root.tar.xz");
+  return 0;
+}
